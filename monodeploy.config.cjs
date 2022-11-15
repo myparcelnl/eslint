85a638ad95a6610da9c6be37ec1135b6ad04d4b1
Edie Lemoine <edie@myparcel.nl>
2022-11-15 14:32:05 +0100

    build: update push flow

--- monodeploy.config.cjs
+++ monodeploy.config.cjs
@@ -10,21 +10,13 @@ const config = {
   autoCommitMessage: 'chore: release [skip ci]',
   changelogFilename: '<packageDir>/CHANGELOG.md',
   conventionalChangelogConfig: 'conventional-changelog-conventionalcommits',
-  git: {
-    baseBranch: '3580814308a03b2efb7b94f9fdd628109e4a1a24',
-  },
   persistVersions: true,
-  plugins: [
-    '@monodeploy/plugin-github',
-  ],
+  plugins: ['@monodeploy/plugin-github'],
   prerelease: prereleaseBranches.includes(branch),
-  prereleaseId: branch === 'next' ? 'rc' : branch,
+  prereleaseId: branch,
   prereleaseNPMTag: branch,
   preset: 'monodeploy/preset-recommended',
   registryUrl: 'https://registry.npmjs.org',
 };
-
-console.log('ℹ️ Detected branch:', branch);
-console.log('ℹ️ Config:', JSON.stringify(config, null, 2));
 
 module.exports = config;
