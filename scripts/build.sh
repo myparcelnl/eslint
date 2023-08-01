a2fd901740f9ee4a536f7672ebf0f46441512697
Edie Lemoine <edie@myparcel.nl>
2023-08-01 09:00:20 +0200

    feat(build): add type declarations

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 echo "module.exports = require('./dist/index.js').config;\n" > index.js
 echo "Created index.js"
 
-tsup-node src/index.ts --minify --target node14 --format cjs
+tsup-node src/index.ts --minify --target node14 --format cjs,esm --dts
